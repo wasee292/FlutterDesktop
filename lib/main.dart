005f7c6404f68b0a2bcc277d71f5385dd9106e4b
wasee292 <smwaseem25698@gmail.com>
2020-02-14 12:10:47 +0530

    Fetch Github Repo list

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
+import 'package:example_flutter/api.dart';
+import 'package:example_flutter/githubItem.dart';
+import 'package:example_flutter/repo.dart';
+import 'package:example_flutter/searchList.dart';
 
 void main() {
-  // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-
   runApp(new MyApp());
 }
 
@@ -27,32 +15,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // See https://github.com/flutter/flutter/wiki/Desktop-shells#fonts
-        fontFamily: 'Roboto',
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  List<Repo> _repos = List();
+  bool _isFetching = false;
+  String _error;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    loadTrendingRepos();
+  }
+
+  void loadTrendingRepos() async {
     setState(() {
-      _counter++;
+      _isFetching = true;
+      _error = null;
+    });
+
+    final repos = await Api.getTrendingRepositories();
+    setState(() {
+      _isFetching = false;
+      if (repos != null) {
+        this._repos = repos;
+      } else {
+        _error = 'Error fetching repos';
+      }
     });
   }
 
@@ -60,27 +60,54 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Container(
+            margin: EdgeInsets.only(top: 4.0),
+            child: Column(
+              children: <Widget>[
+                Text('Github Repos',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .apply(color: Colors.white)),
+                Text('Trending',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1
+                        .apply(color: Colors.white))
+              ],
+            )),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchList()));
+              }),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: buildBody(context),
     );
+  }
+
+  Widget buildBody(BuildContext context) {
+    if (_isFetching) {
+      return Container(
+          alignment: Alignment.center, child: Icon(Icons.timelapse));
+    } else if (_error != null) {
+      return Container(
+          alignment: Alignment.center,
+          child: Text(
+            _error,
+            style: Theme.of(context).textTheme.headline5,
+          ));
+    } else {
+      return ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          itemCount: _repos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GithubItem(_repos[index]);
+          });
+    }
   }
 }
